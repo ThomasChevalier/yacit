c9b25fb4ebd7a6c70bed385516e7edc4494e4d75
Thomas Chevalier <thomasom.chevalier@gmail.com>
2022-05-24 11:47:21 +0200

    Change flags of interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod tun;
-use crate::tun::{tun_create, IFF_TUN, IFF_NO_PI};
+use crate::tun::{tun_create, IFF_TUN, IFF_NO_PI, IFF_UP, if_get_flags, if_set_flags};
 
 fn main() {
-    tun_create("tun0", IFF_TUN | IFF_NO_PI).expect("Cannot create interface tun0");
+    let fd = tun_create("tun0", IFF_TUN | IFF_NO_PI).expect("Cannot create interface tun0");
+
+    let flags = if_get_flags("tun0").expect("Cannot get flags of tun0");
+    if_set_flags("tun0", flags | IFF_UP).expect("Cannot set flags of tun0");
 }
--- src/tun.rs
+++ src/tun.rs
@@ -3,27 +3,53 @@ use ifstructs::ifreq;
 use libc;
 use std::os::unix::prelude::RawFd;
 
-// From https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_tun.h#L66
-pub const IFF_TUN: libc::c_short = 0x0001;
-pub const IFF_NO_PI: libc::c_short = 0x1000;
+// FLAGS
 
-// From https://github.com/torvalds/linux/blob/master/include/uapi/linux/if_tun.h#L34
-const TUN_IOC_MAGIC: u8 = 'T' as u8;
-const TUN_IOC_SET_IFF: u8 = 202;
-const TUN_IOC_SET_PERSIST: u8 = 203;
+// linux/include/uapi/linux/if_tun.h
+pub const IFF_TUN: libc::c_short = libc::IFF_TUN as  libc::c_short;
+pub const IFF_NO_PI: libc::c_short = libc::IFF_NO_PI as  libc::c_short;
 
-unsafe fn tun_set_iff(fd: i32, data: *const ifreq) -> Result<i32, nix::errno::Errno> {
-    let res = libc::ioctl(fd, nix::request_code_write!(TUN_IOC_MAGIC, TUN_IOC_SET_IFF, std::mem::size_of::<i32>()), data);
-    nix::errno::Errno::result(res)
+// linux/include/uapi/linux/if.h
+pub const IFF_UP: libc::c_short = libc::IFF_UP as libc::c_short;
+
+mod ioctl{
+    use ifstructs::ifreq;
+    use std::os::unix::prelude::RawFd;
+
+    // linux/include/uapi/linux/if_tun.h
+    const TUN_IOC_MAGIC: u8 = 'T' as u8;
+    const TUN_IOC_SET_IFF: u8 = 202;
+    const TUN_IOC_SET_PERSIST: u8 = 203;
+
+    pub unsafe fn tun_set_iff(fd: RawFd, data: *const ifreq) -> Result<i32, nix::errno::Errno> {
+        let res = libc::ioctl(fd, nix::request_code_write!(TUN_IOC_MAGIC, TUN_IOC_SET_IFF, std::mem::size_of::<i32>()), data);
+        nix::errno::Errno::result(res)
+    }
+    
+    pub unsafe fn tun_set_persist(fd: RawFd, data: *const bool) -> Result<i32, nix::errno::Errno> {
+        let res = libc::ioctl(fd, nix::request_code_write!(TUN_IOC_MAGIC, TUN_IOC_SET_PERSIST, std::mem::size_of::<i32>()), data);
+        nix::errno::Errno::result(res)
+    }
+    
+    pub unsafe fn if_get_flags(fd: RawFd, data: *const ifreq) -> Result<i32, nix::errno::Errno> {
+        let res = libc::ioctl(fd, libc::SIOCGIFFLAGS, data);
+        nix::errno::Errno::result(res)
+    }
+    
+    pub unsafe fn if_set_flags(fd: RawFd, data: *const ifreq) -> Result<i32, nix::errno::Errno> {
+        let res = libc::ioctl(fd, libc::SIOCSIFFLAGS, data);
+        nix::errno::Errno::result(res)
+    }
 }
 
-unsafe fn tun_set_persist(fd: i32, data: *const bool) -> Result<i32, nix::errno::Errno> {
-    let res = libc::ioctl(fd, nix::request_code_write!(TUN_IOC_MAGIC, TUN_IOC_SET_PERSIST, std::mem::size_of::<i32>()), data);
-    nix::errno::Errno::result(res)
+fn get_dummy_socket() -> Result<RawFd, nix::errno::Errno> {
+    use nix::sys::socket::*;
+    socket(
+        AddressFamily::Inet, SockType::Datagram, 
+        SockFlag::empty(), None)
 }
 
-
-pub fn tun_create(name: &str, flags: libc::c_short) -> Result<(), nix::errno::Errno> {
+pub fn tun_create(name: &str, flags: libc::c_short) -> Result<RawFd, nix::errno::Errno> {
     let fd: RawFd = nix::fcntl::open("/dev/net/tun", nix::fcntl::OFlag::O_RDWR, nix::sys::stat::Mode::empty())?;
 
     let mut req = ifreq::from_name(name).unwrap();
@@ -31,8 +57,27 @@ pub fn tun_create(name: &str, flags: libc::c_short) -> Result<(), nix::errno::Er
     let persist = true;
 
     unsafe{
-        tun_set_iff(fd, &req)?;
-        tun_set_persist(fd, &persist)?;
+        ioctl::tun_set_iff(fd, &req)?;
+        ioctl::tun_set_persist(fd, &persist)?;
+    }
+    Ok(fd)
+}
+
+pub fn if_get_flags(name: &str) -> Result<libc::c_short, nix::errno::Errno> {
+    let req = ifreq::from_name(name).unwrap();
+
+    unsafe{
+        ioctl::if_get_flags(get_dummy_socket()?, &req)?;
+    }
+    Ok(req.get_flags())
+}
+
+pub fn if_set_flags(name: &str, flags: libc::c_short) -> Result<(), nix::errno::Errno> {
+    let mut req = ifreq::from_name(name).unwrap();
+    req.set_flags(flags);
+
+    unsafe{
+        ioctl::if_set_flags(get_dummy_socket()?, &req)?;
     }
     Ok(())
 }
